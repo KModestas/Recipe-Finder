607a790b82f4c177dfa2df1abaa94d3cda803453
Modestas <modestask97@gmail.com>
2018-05-09 11:07:58 +0100

    search component changes

---
+++ src/components/SearchRecipes.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Form, FormGroup, FormControl, ControlLabel, Button } from 'react-bootstrap';
+
+class SearchRecipes extends Component {
+  render() {
+    return(
+      <Form inline>
+        <FormGroup>
+        </FormGroup>
+        {''}
+        <FormGroup>
+        </FormGroup>
+      </Form>
+    );
+  }
+}
+
+export default SearchRecipes;
