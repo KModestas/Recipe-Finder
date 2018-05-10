8f42b16138d555cc3f84b6d4be6ddbba0f8ac226
Modestas <modestask97@gmail.com>
2018-05-10 13:54:41 +0100

    added state properties

--- src/components/SearchRecipes.js
+++ src/components/SearchRecipes.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 import { Form, FormGroup, FormControl, ControlLabel, Button } from 'react-bootstrap';
 
 class SearchRecipes extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      ingredients: '',
+      dish: ''
+    };
+  }
+
+  // seen as this is the only component that will utilise this data, adding a local state is fine
 
   search() {
     const url = 'http://www.recipepuppy.com/api/';
