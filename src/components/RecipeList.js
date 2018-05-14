c1f001b3c48721ada17a3fd78eb48840c365811a
Modestas <modestask97@gmail.com>
2018-05-14 11:18:35 +0100

    mapped state to props on recipe list component

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,2 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+
+class RecipeList extends Component {
+  render() {
+    console.log('this.props', this.props);
+
+    return(
+      <div></div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return state;
+}
+
+export default connect(mapStateToProps, null)(RecipeList);
